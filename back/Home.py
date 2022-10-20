fda0992941615fa16cabdd2e8369259bf87e82a9
Miguel <miguel.elizaldem@outlook.com>
2022-10-20 18:26:25 -0500

    Login medio listo

--- back/Home.py
+++ back/Home.py
@@ -1,23 +1,50 @@
-from distutils.command.clean import clean
+#from distutils.command.clean import clean
+#from multiprocessing import AuthenticationError
 #from json import load
-from unicodedata import name
-import streamlit as st
-import pandas as pd
-from pandasql import sqldf
-import plotly.express as px
+#from unicodedata import name
 
-from pages.Load_file import treated_data
+#import pandas as pd
+#from pandasql import sqldf
+#import plotly.express as px
+#from pages.Performance import display_page
+
+#from pages.Load_file import treated_data
 from PIL import Image
 
+#Required for LOGIN
+import streamlit as st
+import pickle
+from pathlib import Path
+import streamlit_authenticator  as stauth
+import yaml
+
+def login():
+    
+    with open('./config.yaml') as file:
+        config = yaml.load(file, Loader=yaml.SafeLoader)
+        authenticator = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days'],
+            config['preauthorized']
+)
+
+    name, authentication_status, username = authenticator.login('Login', 'main')
+
+    if authentication_status:
+        authenticator.logout('Logout', 'main')
+        st.write(f'Welcome *{name}*')
+        #display()
+    elif authentication_status == False:
+        st.error('Username/password is incorrect')
+    elif authentication_status == None:
+        st.warning('Please enter your username and password')
 
 def main():
-    image = Image.open('Logo-Madison.png')
+    image = Image.open('./back/Logo-Madison.png')
     st.image(image)
-    st.sidebar.write()
+    #st.sidebar.write()
     
-
-   
-    # display_page()
-
-
 main()
+login()
---
+++ back/passwords.py
@@ -0,0 +1,3 @@
+import streamlit_authenticator as stauth
+hashed_passwords = stauth.Hasher(['123']).generate()
+print(hashed_passwords)
