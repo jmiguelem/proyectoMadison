59ef604741334639c1ffc5d33929418e81d8ef29
rolan <rolan@LAPTOP-P60AJPGQ>
2022-10-21 09:44:16 -0500

    Cambio de colores en graficas

--- back/pages/Performance.py
+++ back/pages/Performance.py
@@ -7,27 +7,26 @@ from pages.Load_file import ss
 # Pagina de Display
 def display_page():
     # Lista de materias
-    materias = ('Language Acquisition - English',
-                'Individuos y Sociedades - Formación cívica y ética',
-                'Individuos y Sociedades - Historia de México',
-                'Language Acquisition -  French', 'Lengua y Literatura - Español',
-                'Mathematics', 'Physical and health education',
-                'Sciences - Chemistry', 'Technology Education', 'Arts - Music',
-                'Individuals and Societies - World History',
-                'Lengua y Literatura - Literatura', 'Design', 'Community Project',
-                'Arts - Visual Arts', 'Individuos y Sociedades - Geografía',
-                'Individuals and societies - World History', 'Sciences - Physics',
-                'Visual Arts', 'Sciences - Biology',
-                'Sciences - Integrated Science')
-
+    materias = {'Language Acquisition - English':'blue',
+                'Individuos y Sociedades - Formación cívica y ética':'pink',
+                'Individuos y Sociedades - Historia de México':'yellow',
+                'Language Acquisition -  French':'purple', 'Lengua y Literatura - Español':'orange',
+                'Mathematics':'red', 'Physical and health education':'black',
+                'Sciences - Chemistry':'green', 'Technology Education':'black', 'Arts - Music':'black',
+                'Individuals and Societies - World History':'yellow',
+                'Lengua y Literatura - Literatura':'light blue', 'Design':'black', 'Community Project':'black',
+                'Arts - Visual Arts':'black', 'Individuos y Sociedades - Geografía':'light green',
+                'Individuals and societies - World History':'yellow', 'Sciences - Physics':'green',
+                'Visual Arts':'black', 'Sciences - Biology':'green',
+                'Sciences - Integrated Science':'green'}
     
 
    
     #st.session_state['Treated_grades'] 
     if 'Treated_grades' in st.session_state:
         # Display de un select box con la lista de materias
-        materia = st.selectbox("Materias", materias, key='2')
-
+        materia = st.selectbox("Materias", materias.keys(), key='2')
+        
         df = st.session_state['Treated_grades']
         # st.write(df)
         # Selecionamos la materia que queremos
@@ -35,13 +34,13 @@ def display_page():
 
         # Histograma SEP
         SEP = px.histogram(df, x="SEP", color_discrete_sequence=[
-            'blue', 'blue', 'green'])
+            materias[materia]])
         SEP.update_layout(title=materia,
                         xaxis_title_text='Calificacones SEP',
                         yaxis_title_text='Cantidad de alumnos')
         # Histograma IB
         IB = px.histogram(df, x="Grade", color_discrete_sequence=[
-            'indianred'], nbins=31)
+            materias[materia]], nbins=31)
         IB.update_layout(title=materia,
                         xaxis_title_text='Calificacones IB',
                         yaxis_title_text='Cantidad de alumnos')
